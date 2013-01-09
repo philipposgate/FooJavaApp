00398ffdfef0690ded114efd2c29580677fc3a23
philipposgate <philip.posgate@gmail.com>
2013-01-08 22:35:26 -0500

    getTime

--- src/foo/Main.java
+++ src/foo/Main.java
@@ -6,6 +6,6 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World! " + new Date());
+		System.out.println("Hello World! " + new Date().getTime());
 	}
 }
