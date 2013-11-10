53dedb1cd02dbd1488c0142aaaf548d4abb1a77a
philipposgate <philip.posgate@gmail.com>
2013-11-09 20:59:46 -0500

    eeee

--- src/foo/Main.java
+++ src/foo/Main.java
@@ -6,7 +6,9 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		// A comment....1234
+		// A comment....123
+		
+		// branchTest1: added this comment!
 		System.out.println("Hello World! " + new Date().getTime());
 	}
 }
