ec10e9b1ee1053a9474de42c86e481bd0e028bd5
philipposgate <philip.posgate@gmail.com>
2013-11-09 19:45:47 -0500

    first change

--- src/foo/Main.java
+++ src/foo/Main.java
@@ -7,6 +7,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		// A comment....123
+		// branchTest1: added this comment!
 		System.out.println("Hello World! " + new Date().getTime());
 	}
 }
