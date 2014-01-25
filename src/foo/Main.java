45a097e581c4e360ebfe0a56b1e309e6ff4a487a
Philip Posgate <philip.posgate@gmail.com>
2014-01-25 13:48:55 -0800

    added ... to Main.java

--- src/foo/Main.java
+++ src/foo/Main.java
@@ -7,7 +7,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		// A comment....1234
-		// another-branch: added this comment :)
+		// another-branch: added this comment :) ...
 		// yet-another-branch: added this comment :)
 		System.out.println("Hello World! " + new Date().getTime());
 	}
